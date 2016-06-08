cac5728ae52fd7e7c4e43f57bf1b2853ff9f92c1
Shahab Layeghi <shahab.layeghi@intel.com>
2016-06-08 12:24:47 -0700

    starting to add api

--- roomserver.js
+++ roomserver.js
@@ -1,51 +1,63 @@
+/* globals require, console, */
 var WebSocketServer = require('websocket').server;
 var http = require('http');
  
 var server = http.createServer(function(request, response) {
-    console.log((new Date()) + ' Received request for ' + request.url);
-    response.writeHead(404);
-    response.end();
+  console.log((new Date()) + ' Received request for ' + request.url);
+  response.writeHead(404);
+  response.end();
 });
 server.listen(8080, function() {
-    console.log((new Date()) + ' Server is listening on port 8080');
+  console.log((new Date()) + ' Server is listening on port 8080');
 });
  
-wsServer = new WebSocketServer({
-    httpServer: server,
-    // You should not use autoAcceptConnections for production 
-    // applications, as it defeats all standard cross-origin protection 
-    // facilities built into the protocol and the browser.  You should 
-    // *always* verify the connection's origin and decide whether or not 
-    // to accept it. 
-    autoAcceptConnections: false
+var wsServer = new WebSocketServer({
+  httpServer: server,
+  // You should not use autoAcceptConnections for production 
+  // applications, as it defeats all standard cross-origin protection 
+  // facilities built into the protocol and the browser.  You should 
+  // *always* verify the connection's origin and decide whether or not 
+  // to accept it. 
+  autoAcceptConnections: false
 });
  
 function originIsAllowed(origin) {
-  // put logic here to detect whether the specified origin is allowed. 
+ // put logic here to detect whether the specified origin is allowed. 
   return true;
 }
  
 wsServer.on('request', function(request) {
-    if (!originIsAllowed(request.origin)) {
-      // Make sure we only accept requests from an allowed origin 
-      request.reject();
-      console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
-      return;
-    }
+  var jsonMessage;
+  var regiesteredRooms = [];
+  if (!originIsAllowed(request.origin)) {
+    // Make sure we only accept requests from an allowed origin 
+    request.reject();
+    console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
+    return;
+  }
     
-    var connection = request.accept('echo-protocol', request.origin);
-    console.log((new Date()) + ' Connection accepted.');
-    connection.on('message', function(message) {
-        if (message.type === 'utf8') {
-            console.log('Received Message: ' + message.utf8Data);
-            connection.sendUTF(message.utf8Data);
-        }
-        else if (message.type === 'binary') {
-            console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
-            connection.sendBytes(message.binaryData);
-        }
-    });
-    connection.on('close', function(reasonCode, description) {
-        console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
-    });
+  var connection = request.accept('echo-protocol', request.origin);
+  console.log((new Date()) + ' Connection accepted.');
+  connection.on('message', function(message) {
+    if (message.type === 'utf8') {
+      console.log('Received Message: ' + message.utf8Data);
+      jsonMessage = JSON.parse(message.utf8Data);
+      if(jsonMessage.type == 'REGISTER') {
+        regiesteredRooms.push(jsonMessage.room);
+        console.log('room registered: ' + jsonMessage.room);
+        connection.sendUTF(JSON.stringify({'type': 'REGISTER_RESPONSE', 'result': true, 'requestId': jsonMessage.requestID}));
+      }
+      if(jsonMessage.type == 'GET_ROOM_LIST') {
+        console.log('Get Room List recieved');
+        connection.sendUTF(JSON.stringify({'type': 'ROOM_LIST', 'list': regiesteredRooms}));
+      }
+    }
+    /*else if (message.type === 'binary') {
+      console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
+      connection.sendBytes(message.binaryData);
+    }*/
+  });
+  connection.on('close', function(reasonCode, description) {
+    console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
+  });
 });
--- testclient.js
+++ testclient.js
@@ -1,33 +1,36 @@
+
+/* globals require, console, setTimeout */
 var WebSocketClient = require('websocket').client;
  
 var client = new WebSocketClient();
  
 client.on('connectFailed', function(error) {
-    console.log('Connect Error: ' + error.toString());
+  console.log('Connect Error: ' + error.toString());
 });
- 
+
 client.on('connect', function(connection) {
-    console.log('WebSocket Client Connected');
-    connection.on('error', function(error) {
-        console.log("Connection Error: " + error.toString());
-    });
-    connection.on('close', function() {
-        console.log('echo-protocol Connection Closed');
-    });
-    connection.on('message', function(message) {
-        if (message.type === 'utf8') {
-            console.log("Received: '" + message.utf8Data + "'");
-        }
-    });
-    
-    function sendNumber() {
-        if (connection.connected) {
-            var number = Math.round(Math.random() * 0xFFFFFF);
-            connection.sendUTF(number.toString());
-            setTimeout(sendNumber, 1000);
-        }
+  console.log('WebSocket Client Connected');
+  connection.on('error', function(error) {
+    console.log("Connection Error: " + error.toString());
+  });
+  connection.on('close', function() {
+    console.log('echo-protocol Connection Closed');
+  });
+  connection.on('message', function(message) {
+    if (message.type === 'utf8') {
+      console.log("Received: '" + message.utf8Data + "'");
     }
-    sendNumber();
+  });
+
+  function sendCommand() {
+    if (connection.connected) {
+      var number = Math.round(Math.random() * 0xFFFFFF);
+      var registerCommand = {'type': 'REGISTER', 'requestID': number, 'room': 1};
+      connection.sendUTF(JSON.stringify(registerCommand));
+      setTimeout(sendCommand, 1000);
+    }
+  }
+  sendCommand();
 });
  
 client.connect('ws://localhost:8080/', 'echo-protocol');
